3848f762395a3606dea5b6970291e949bcb4fa2d
Nicolai <nicolai@mmg.io>
2017-01-14 13:30:15 +0000

    Colour change experiment

---
+++ app.js
@@ -0,0 +1,8 @@
+const myHeading = document.getElementById('myHeading');
+const myButton = document.getElementById('myButton');
+const myTextInput = document.getElementById('myTextInput');
+
+myButton.addEventListener('click', () => {
+  myButton.style.color = myTextInput.value;
+});
+
